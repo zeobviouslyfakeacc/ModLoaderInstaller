25e8ab43fbc9540070d0171a257f203716f21faf
zeobviouslyfakeacc <zeobviouslyfakeacc@users.noreply.github.com>
2018-02-26 01:37:34 +0100

    Add previously missing assembly info file

---
+++ src/csharp/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ModLoader")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ModLoader")]
+[assembly: AssemblyCopyright("Fuck Copyright Law")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("44f28c87-bed3-4899-9824-2ffd0875f546")]
+[assembly: AssemblyVersion("1.3")]
+[assembly: AssemblyFileVersion("1.3")]
