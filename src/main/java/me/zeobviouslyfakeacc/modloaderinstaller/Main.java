266f3a4deef392f81bbd4e0de76f0cc02e7e8e23
zeobviouslyfakeacc <zeobviouslyfakeacc@users.noreply.github.com>
2018-12-17 22:36:31 +0100

    Split Main and MainApplication for better compatibility with JDK 11

--- src/main/java/me/zeobviouslyfakeacc/modloaderinstaller/Main.java
+++ src/main/java/me/zeobviouslyfakeacc/modloaderinstaller/Main.java
@@ -1,32 +1,14 @@
 package me.zeobviouslyfakeacc.modloaderinstaller;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.stage.Stage;
 
-public class Main extends Application {
+public class Main {
 
 	public static void main(String[] args) {
 		if (OperatingSystem.getCurrent() == OperatingSystem.LINUX) {
 			System.setProperty("prism.lcdtext", "false");
 		}
 
-		Application.launch(Main.class, args);
-	}
-
-	@Override
-	public void start(Stage primaryStage) throws Exception {
-		Parent root = FXMLLoader.load(Main.class.getResource("MainPanel.fxml"));
-		Scene scene = new Scene(root);
-		primaryStage.setScene(scene);
-
-		primaryStage.getIcons().add(new Image(Main.class.getResourceAsStream("/icon.png")));
-		primaryStage.sizeToScene();
-		primaryStage.setResizable(false);
-		primaryStage.setTitle("TLD Mod Loader Installer");
-		primaryStage.show();
+		Application.launch(MainApplication.class, args);
 	}
 }
---
+++ src/main/java/me/zeobviouslyfakeacc/modloaderinstaller/MainApplication.java
@@ -0,0 +1,24 @@
+package me.zeobviouslyfakeacc.modloaderinstaller;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class MainApplication extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		Parent root = FXMLLoader.load(Main.class.getResource("MainPanel.fxml"));
+		Scene scene = new Scene(root);
+		primaryStage.setScene(scene);
+
+		primaryStage.getIcons().add(new Image(Main.class.getResourceAsStream("/icon.png")));
+		primaryStage.sizeToScene();
+		primaryStage.setResizable(false);
+		primaryStage.setTitle("TLD Mod Loader Installer");
+		primaryStage.show();
+	}
+}
