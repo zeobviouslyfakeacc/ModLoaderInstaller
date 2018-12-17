323bf758686ef999f12ef63f82d59c50a46367b9
zeobviouslyfakeacc <zeobviouslyfakeacc@users.noreply.github.com>
2018-12-17 23:07:03 +0100

    Update Rewired patch, ModLoader DLL, and Constants

--- src/main/java/me/zeobviouslyfakeacc/modloaderinstaller/Constants.java
+++ src/main/java/me/zeobviouslyfakeacc/modloaderinstaller/Constants.java
@@ -9,10 +9,10 @@ public final class Constants {
 	public static final String TLD_VERSION = "v1.41";
 	public static final String DLL_NAME = "Rewired_Core.dll";
 	public static final String XOR_NAME = "Rewired_Core.xor";
-	public static final long UNPATCHED_SIZE = 1861632L;
-	public static final long PATCHED_SIZE = 1862144L;
-	public static final String UNPATCHED_SHA1 = "e2864e453b70b1e61ddf2f6382a49d9432701e1f";
-	public static final String PATCHED_SHA1 = "e7d8a1c4ba2fc78bfd737a58d62466436af09854";
-	public static final String MOD_LOADER_SHA1 = "75751832efb204e0cb2a1abcb69b18c80b11d5a4";
+	public static final long UNPATCHED_SIZE = 2214912L;
+	public static final long PATCHED_SIZE = 2214912L;
+	public static final String UNPATCHED_SHA1 = "25d0d80a7c88f813cda4d03e6e9b7d85568372b0";
+	public static final String PATCHED_SHA1 = "7c107c4234d7fee2759f4d4ea592fb5d94c8b67a";
+	public static final String MOD_LOADER_SHA1 = "f16020a95893b42b358bbc8461117b1c84516592";
 	public static final String ERROR_SHA1 = "- ERROR -";
 }
