90d02298291f0cd65aaed35798912680fada0e0f
zeobviouslyfakeacc <zeobviouslyfakeacc@users.noreply.github.com>
2019-05-08 03:22:15 +0200

    Update Rewired_Core patch, rebuild ModLoader, set TLD min ver to v1.48.1

--- src/main/java/me/zeobviouslyfakeacc/modloaderinstaller/Constants.java
+++ src/main/java/me/zeobviouslyfakeacc/modloaderinstaller/Constants.java
@@ -6,13 +6,13 @@ public final class Constants {
 		throw new Error("No instances!");
 	}
 
-	public static final String TLD_VERSION = "v1.41";
+	public static final String TLD_VERSION = "v1.48.1";
 	public static final String DLL_NAME = "Rewired_Core.dll";
 	public static final String XOR_NAME = "Rewired_Core.xor";
-	public static final long UNPATCHED_SIZE = 2214912L;
-	public static final long PATCHED_SIZE = 2214912L;
-	public static final String UNPATCHED_SHA1 = "25d0d80a7c88f813cda4d03e6e9b7d85568372b0";
-	public static final String PATCHED_SHA1 = "7c107c4234d7fee2759f4d4ea592fb5d94c8b67a";
-	public static final String MOD_LOADER_SHA1 = "30d71bb15437154604cf38d759b87a92cc7a813a";
+	public static final long UNPATCHED_SIZE = 2192384L;
+	public static final long PATCHED_SIZE = 2192384L;
+	public static final String UNPATCHED_SHA1 = "28a347150ecbcc4c35629aa5ae645f6593c162ac";
+	public static final String PATCHED_SHA1 = "0d9236b7fd6384a3adbb742bfc04ad00b2e753eb";
+	public static final String MOD_LOADER_SHA1 = "12535174dbd5e21755d0b319ab4da0ce71c827a2";
 	public static final String ERROR_SHA1 = "- ERROR -";
 }
