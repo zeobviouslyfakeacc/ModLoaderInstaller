3ec9580e87796743bf932ea4597abc5bc49e6fc0
zeobviouslyfakeacc <zeobviouslyfakeacc@users.noreply.github.com>
2019-09-14 15:25:54 +0200

    Rebuild ModLoader.dll and set TLD version to 1.56

--- src/main/java/me/zeobviouslyfakeacc/modloaderinstaller/Constants.java
+++ src/main/java/me/zeobviouslyfakeacc/modloaderinstaller/Constants.java
@@ -6,13 +6,13 @@ public final class Constants {
 		throw new Error("No instances!");
 	}
 
-	public static final String TLD_VERSION = "v1.48.1";
+	public static final String TLD_VERSION = "v1.56";
 	public static final String DLL_NAME = "Rewired_Core.dll";
 	public static final String XOR_NAME = "Rewired_Core.xor";
 	public static final long UNPATCHED_SIZE = 2192384L;
 	public static final long PATCHED_SIZE = 2192384L;
 	public static final String UNPATCHED_SHA1 = "28a347150ecbcc4c35629aa5ae645f6593c162ac";
 	public static final String PATCHED_SHA1 = "0d9236b7fd6384a3adbb742bfc04ad00b2e753eb";
-	public static final String MOD_LOADER_SHA1 = "12535174dbd5e21755d0b319ab4da0ce71c827a2";
+	public static final String MOD_LOADER_SHA1 = "00c93dfea9890425834ec2929e33e99efcc3eada";
 	public static final String ERROR_SHA1 = "- ERROR -";
 }
